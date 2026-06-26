e24b9fbb9e8938c5b5706f47737dc70d14a74eca
pangfd <pangfudong@huawei.com>
2026-06-26 09:17:41 +0800

    C语言指针编程课程
    Co-authored-by: pangfudong<pangfudong@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !111 merge test into test
    
    C语言指针编程课程
    
    Created-by: pangfd
    Commit-by: pangfudong
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 内容优化
    - [ ] 其他，请描述：
    
    ## 其他信息
    <!-- 在这里可以添加任何与这个 Pull Request 相关的其他说明。 -->
    
    See merge request: cann/cann-learning-hub!111

---
+++ tutorials/ascendc_operator_development_V2/03_programming_model/answer/03.03.04/add/add.h
@@ -0,0 +1,60 @@
+#include "c_api/asc_simd.h"
+
+constexpr uint32_t LOOP_COUNT = 1;
+constexpr uint32_t WIDTH = 128;
+constexpr uint32_t SINGLE_VF_DATA_LEN = WIDTH;
+
+__simd_vf__ inline void add_vf(__ubuf__ float* dst_ub, __ubuf__ float* src0_ub, __ubuf__ float* src1_ub)
+{
+    constexpr uint16_t one_repeat_cnt = asc_get_vf_len() / sizeof(float);
+    uint16_t repeat_times = (WIDTH + one_repeat_cnt - 1) / one_repeat_cnt;
+    vector_bool mask_full = asc_create_mask_b32(PAT_ALL);
+
+    vector_float src0_reg;
+    vector_float src1_reg;
+    vector_float dst_reg;
+
+    uint32_t count = WIDTH;
+    for (uint16_t i = 0; i < repeat_times; i++) {
+        asc_loadalign(src0_reg, src0_ub + i * one_repeat_cnt);
+        asc_loadalign(src1_reg, src1_ub + i * one_repeat_cnt);
+        asc_add(dst_reg, src0_reg, src1_reg, mask_full);
+        asc_storealign(dst_ub + i * one_repeat_cnt, dst_reg, mask_full);
+    }
+}
+
+// ======================= 核函数 =========================
+__global__ __vector__ void add_custom(__gm__ uint8_t* src0, __gm__ uint8_t* src1, __gm__ uint8_t* dst)
+{
+    asc_init();
+
+    // 申请UB内存
+    __ubuf__ float src0_ub[128];
+    __ubuf__ float src1_ub[128];
+    __ubuf__ float dst_ub[128];
+
+    __gm__ float* src0_gm = reinterpret_cast<__gm__ float*>(src0) + get_block_idx() * SINGLE_VF_DATA_LEN * LOOP_COUNT;
+    __gm__ float* src1_gm = reinterpret_cast<__gm__ float*>(src1) + get_block_idx() * SINGLE_VF_DATA_LEN * LOOP_COUNT;
+    __gm__ float* dst_gm = reinterpret_cast<__gm__ float*>(dst) + get_block_idx() * SINGLE_VF_DATA_LEN * LOOP_COUNT;
+
+    for (uint32_t i = 0; i < LOOP_COUNT; i++) {
+        // 上一个循环的数据搬出操作完成后才能启动新的数据搬入操作
+        uint8_t mutex_id = 1;
+        asc_lock(PIPE_MTE2, mutex_id);
+        asc_set_copy_pad_val(float(0.0));
+        asc_copy_gm2ub_align(src0_ub, &src0_gm[i * SINGLE_VF_DATA_LEN], 1, static_cast<uint16_t>(WIDTH * sizeof(float)), 0, 0, true, 0, 0, 0);
+        asc_copy_gm2ub_align(src1_ub, &src1_gm[i * SINGLE_VF_DATA_LEN], 1, static_cast<uint16_t>(WIDTH * sizeof(float)), 0, 0, true, 0, 0, 0);
+        asc_unlock(PIPE_MTE2, mutex_id);
+
+        // 数据搬入操作完成后才能启动计算
+        asc_lock(PIPE_V, mutex_id);
+        // 调用VF函数执行计算
+        add_vf(dst_ub, src0_ub, src1_ub);
+        asc_unlock(PIPE_V, mutex_id);
+
+        // 计算完成后才能启动数据搬出
+        asc_lock(PIPE_MTE3, mutex_id);
+        asc_copy_ub2gm_align(&dst_gm[i * SINGLE_VF_DATA_LEN], dst_ub, 1, static_cast<uint16_t>(WIDTH * sizeof(float)), 0, 0, 0);
+        asc_unlock(PIPE_MTE3, mutex_id);
+    }
+}
