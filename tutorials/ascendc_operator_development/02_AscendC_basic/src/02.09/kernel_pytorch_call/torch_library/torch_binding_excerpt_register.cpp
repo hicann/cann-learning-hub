50659dcca0bda46e195b120e14c9816759e6cfdc
YANXI_ZHAO <zhaoyanxi3@hisilicon.com>
2026-07-09 19:47:24 +0800

    resume 02_AscendC_basic
    Co-authored-by: YANXI_ZHAO<zhaoyanxi3@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !306 merge test into test
    
    resume 02_AscendC_basic
    
    Created-by: YANXI_ZHAO
    Commit-by: YANXI_ZHAO
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 内容优化
    - [ ] 其他，请描述：
    
    ## 其他信息
    <!-- 在这里可以添加任何与这个 Pull Request 相关的其他说明。 -->
    
    See merge request: cann/cann-learning-hub!306

--- tutorials/ascendc_operator_development/02_AscendC_basic/src/02.09/kernel_pytorch_call/torch_library/torch_binding_excerpt_register.cpp
+++
@@ -1,9 +0,0 @@
-TORCH_LIBRARY(ascendc_ops, m)
-{
-    m.def("ascendc_add(Tensor x, Tensor y) -> Tensor");
-}
-
-TORCH_LIBRARY_IMPL(ascendc_ops, PrivateUse1, m)
-{
-    m.impl("ascendc_add", TORCH_FN(ascendc_ops::ascendc_add));
-}
